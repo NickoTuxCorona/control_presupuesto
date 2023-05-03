813aa59d1174f84c627e3a7c1fc60857c5c03b94
Orlando Tuxpan Corona <ntuxcorona@gmail.com>
2023-05-03 11:39:57 -0600

    Creación de los modelos, temporalmente 2 para hacer pruebas

--- presupuesto/models.py
+++ presupuesto/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    name = models.CharField(max_length=255)
+    limit = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+
+
+class Transaccion(models.Model):
+    description = models.CharField(max_length=255)
+    category = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField()
+    ignore = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.description
