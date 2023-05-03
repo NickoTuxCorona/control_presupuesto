210c8cb179b2e15548d4a37bbabda5126e3a2646
Orlando Tuxpan Corona <ntuxcorona@gmail.com>
2023-05-03 15:13:11 -0600

    Ajuste de lenguaje y aplicaciones integradas

--- control_presupuesto/settings.py
+++ control_presupuesto/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'presupuesto',
+    'interfaces',
 ]
 
 MIDDLEWARE = [
@@ -111,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'localtime'
 
 USE_I18N = True
 
