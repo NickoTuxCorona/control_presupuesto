8346cc9d8e87b82fcd656c4018fe91d7c2af7f67
Orlando Tuxpan Corona <ntuxcorona@gmail.com>
2023-05-03 16:19:09 -0600

    vista detalle categoría

--- interfaces/urls.py
+++ interfaces/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import gastos_por_categoria
+from .views import gastos_por_categoria,detalle_categoria
 
 app_name = 'interfaces'
 
 urlpatterns = [
     path('gastos-por-categoria/', gastos_por_categoria, name='gastos_por_categoria'),
+    path('detalle-categoria/<int:categoria_id>/', detalle_categoria, name='detalle_categoria'),
 ]
--- interfaces/views.py
+++ interfaces/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from presupuesto.models import Categoria,Transaccion
+from django.db.models import Sum
 
 def gastos_por_categoria(request):
     categorias = Categoria.objects.all()
@@ -11,3 +12,31 @@ def gastos_por_categoria(request):
         else:
             transacciones = Transaccion.objects.filter(category=categoria.id).count()
     return render(request, 'gastos_por_categoria.html', {'categorias': categorias})
+
+
+def detalle_categoria(request, categoria_id):
+    categoria = get_object_or_404(Categoria, pk=categoria_id)
+    transacciones = Transaccion.objects.filter(category=categoria)
+
+    importe_total_gastado = transacciones.aggregate(Sum('amount'))['amount__sum'] or 0
+    cantidad_movimientos = transacciones.count()
+
+    # Si la categoria tiene presupuesto, calcula el importe límite y disponible
+    if categoria.limit:
+        importe_limite_presupuesto = categoria.limit
+        importe_disponible = importe_limite_presupuesto - importe_total_gastado
+        porcentaje_gastado = (importe_total_gastado / importe_limite_presupuesto) * 100
+    else:
+        importe_limite_presupuesto = 0
+        importe_disponible = 0
+        porcentaje_gastado = 0
+
+    context = {
+        'categoria': categoria,
+        'importe_total_gastado': importe_total_gastado,
+        'cantidad_movimientos': cantidad_movimientos,
+        'importe_limite_presupuesto': importe_limite_presupuesto,
+        'importe_disponible': importe_disponible,
+        'porcentaje_gastado': porcentaje_gastado,
+    }
+    return render(request, 'detalle_categoria.html', context)
